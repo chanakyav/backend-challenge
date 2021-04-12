e8b54a63569a75c2499878fe26e55b4efd82f686
Chanakya <chanakya.valluri@gmail.com>
2021-04-11 23:19:10 -0400

    Add flow for calculating balance

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,32 +9,67 @@ const FILTER_WITH_ADDRESS_AND_SPENT: &str = "address = :address_value and spent 
 async fn main() {
     let client = DynamoDbClient::new(Region::UsEast1);
 
-    println!("scan items for address and spent");
-    let mut address_value: AttributeValue = Default::default();
-    address_value.s = Some("335bRXoRfGcXAdoHi57MpRmWDPgChAPncn".to_string());
-    let mut spent_value: AttributeValue = Default::default();
-    spent_value.bool = Some(true);
+    let address = String::from("1CL5TbB2MaR4mrFjtYQ5GyA3cP2bSmPxAn".to_string());
+    let spent = true;
+    let scan_input = create_scan_input(address, spent);
 
-    let mut values: HashMap<String, AttributeValue> = HashMap::new();
-    values.insert(":address_value".to_string(), address_value);
-    values.insert(":spent_value".to_string(), spent_value);
-    let mut scan_input: ScanInput = Default::default();
-    scan_input.table_name = String::from(TABLE_NAME);
-    scan_input.filter_expression = Some(String::from(FILTER_WITH_ADDRESS_AND_SPENT));
-    scan_input.expression_attribute_values = Some(values);
-
+    let mut items: Vec<HashMap<String, AttributeValue>> = Vec::new();
     match client.scan(scan_input).await {
-        Ok(output) => match output.items {
-            Some(item_list) => {
-                println!("Item count: {}", item_list.len());
-                for item in item_list {
-                    println!("Item: {:?}", item);
+        Ok(output) => {
+            match output.items {
+                Some(item_list) => {
+                    for item in item_list {
+                        items.push(item);
+                    }
                 }
+                None => println!("No transactions found with the given address and spent values"),
             }
-            None => println!("No transactions found with the given address and spent values"),
-        },
+            match output.last_evaluated_key {
+                Some(key) => {
+                    println!("Last evaluated key: {:?}", key)
+                }
+                None => println!("No more data to be retrieved"),
+            }
+        }
         Err(error) => {
             println!("Error: {:?}", error);
         }
     }
+
+    println!("Balance: {}", calculate_total_balance(items));
+}
+
+fn create_scan_input(address: String, spent: bool) -> ScanInput {
+    let mut address_value: AttributeValue = Default::default();
+    address_value.s = Some(address);
+    let mut spent_value: AttributeValue = Default::default();
+    spent_value.bool = Some(spent);
+
+    let mut values: HashMap<String, AttributeValue> = HashMap::new();
+    values.insert(":address_value".to_string(), address_value);
+    values.insert(":spent_value".to_string(), spent_value);
+
+    let mut scan_input: ScanInput = Default::default();
+    scan_input.table_name = String::from(TABLE_NAME);
+    scan_input.filter_expression = Some(String::from(FILTER_WITH_ADDRESS_AND_SPENT));
+    scan_input.expression_attribute_values = Some(values);
+    scan_input.projection_expression = Some("amount".to_string());
+
+    scan_input
+}
+
+fn calculate_total_balance(items: Vec<HashMap<String, AttributeValue>>) -> f64 {
+    let mut amount: f64 = 0.0;
+    for item in items {
+        match item.get(&"amount".to_string()) {
+            Some(amt) => match &amt.n {
+                Some(val) => {
+                    amount = amount + val.parse::<f64>().unwrap();
+                }
+                None => println!("No amount value"),
+            },
+            None => println!("No attribute value"),
+        }
+    }
+    amount
 }
